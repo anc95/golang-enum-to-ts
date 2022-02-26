2847738ea12bf21ae188310d684264d2f1dd8f37
anc95 <1481988258@qq.com>
2022-02-26 12:05:09 +0800

    feat: export Transform func

--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
-	// nothing now
+import (
+	"github.com/anc95/golang-enum-to-ts/src/ast"
+	"github.com/anc95/golang-enum-to-ts/src/generator"
+	"github.com/anc95/golang-enum-to-ts/src/token"
+)
+
+func Transform(s string) string {
+	parser := token.NewParser(s)
+	tokens := parser.Parse()
+
+	astGenerator := ast.NewAstGenerator(tokens)
+	return generator.GenerateTS(astGenerator.Gen())
 }
